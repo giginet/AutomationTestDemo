84c511d35da637b6b3687e4c030b2bbfcd6e335b
giginet <giginet.net@gmail.com>
2013-09-17 23:51:28 +0900

    Modify failed message.

--- specs/javascript/tap_text_view.js
+++ specs/javascript/tap_text_view.js
@@ -3,5 +3,5 @@ test('Tap switch on second page.', function(target, app) {
   createImageAsserter('../../tuneup_js',  './',  '../../specs/image');
   target.frontMostApp().mainWindow().textViews()[0].tap();
   target.delay(1.0);
-  assertScreenMatchesImageNamed("tap_text_view", "a screenshot of second page is not matched.", 1471);
+  assertScreenMatchesImageNamed("tap_text_view", "a screenshot of text view is not matched.", 1471);
 });
